36204fe4bcca15e46a032cdf05f158b2b9463359
Ryo <sekaiitousama@gmail.com>
2021-01-24 20:34:46 +0900

    Revert "deploy先を変更"
    This reverts commit fff844c2481928fd9f7d6fb7c997e84a510b5402.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:ryot3827/count_game.git"
 set :branch, :develop
 
 # Deploy to the user's home directory
-set :deploy_to, "/var/www/#{fetch :application}"
+set :deploy_to, "/home/ubuntu/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
